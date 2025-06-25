38263d996f7d5c5f4f6e4a3547513a2749e1a6a0
Wisam Abdo <wisam.abdo@accenture.com>
2025-06-25 13:56:00 +0100

    Pulled more data from the dataset. Due to error in the dataset more time will need to be put on extraction to ensure code is fully dynamic and can handle all data.

--- src/etl_pipeline/extract_from_snowflake.py
+++ src/etl_pipeline/extract_from_snowflake.py
@@ -6,7 +6,7 @@ from utils.create_db_connection import connect_to_snowflake
 
 
 def get_optimal_pit_stop_data(conn):
-    query = f"""SELECT * FROM optimal_pit_stop_model limit 5500;"""
+    query = f"""SELECT * FROM optimal_pit_stop_model limit 40000;"""
     opt_ps_data = pd.read_sql_query(query, conn)
     return opt_ps_data
 
